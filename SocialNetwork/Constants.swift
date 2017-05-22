29d7a09f7db4bfd1fa7bc7b2b0017530be14e316
ATrueNovice <sean.haughtonscott@gmail.com>
2017-05-22 07:21:28 -0400

    Set up couple of views

---
+++ SocialNetwork/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constance.swift
+//  SocialNetwork
+//
+//  Created by New on 5/22/17.
+//  Copyright © 2017 HSI. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
---
+++ SocialNetwork/FBView.swift
@@ -0,0 +1,22 @@
+//
+//  FBView.swift
+//  SocialNetwork
+//
+//  Created by New on 5/22/17.
+//  Copyright © 2017 HSI. All rights reserved.
+//
+
+import UIKit
+
+class FBView: UIView {
+
+    override func awakeFromNib() {
+        super .awakeFromNib()
+
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+
+}
