fa429e4ee1875dc38f47b3f5f6dd34d4c2750708
ATrueNovice <sean.haughtonscott@gmail.com>
2017-05-25 14:38:24 -0400

    Finished email authentication code setup. NO dismissal of keyboard or next page

--- SocialNetwork/SignInVC.swift
+++ SocialNetwork/SignInVC.swift
@@ -13,6 +13,9 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyText!
+    @IBOutlet weak var passwordField: FancyText!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,7 +31,7 @@ class SignInVC: UIViewController {
 
         facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
             if error != nil {
-            print("NOTE: Unable to Authenticate")
+            print("NOTE: Unable to FB Authenticate")
 
             } else if result?.isCancelled == true {
                 print("NOTE: User Cancelled Authentication")
@@ -52,4 +55,26 @@ class SignInVC: UIViewController {
             }
         })
     }
+    //Email Authentication code
+    @IBAction func SignInTapped(_ sender: Any) {
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil{
+                    print("NOTE: Email User Authenticated W/ Firebase")
+
+                } else {
+                    FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil{
+                            print("NOTE: Unable To Authenticate Email User w/ Firebase")
+
+                        } else {
+                            print("NOTE: Successfully User Authenticated W/ Firebase")
+                        }
+                })
+        }
+
+    })
     }
+}
+
+}
