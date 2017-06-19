9e0a7cdf27833c1dae45c353e393eac226fcb35b
ATrueNovice <sean.haughtonscott@gmail.com>
2017-06-18 23:44:36 -0400

    UI Feed, new custom classes for buttons, constrants, table view created.

--- SocialNetwork/CircleView.swift
+++ SocialNetwork/CircleView.swift
@@ -1,24 +1,28 @@
 //
-//  FBView.swift
+//  CircleView.swift
 //  SocialNetwork
 //
-//  Created by New on 5/22/17.
+//  Created by New on 6/18/17.
 //  Copyright © 2017 HSI. All rights reserved.
 //
 
 import UIKit
 
-class FBView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
-        super .awakeFromNib()
-
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-
     }
+
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+
+
 
 }
