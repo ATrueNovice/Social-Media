36f74edfa2733b53f51b44de2bc84fc2190641a0
ATrueNovice <sean.haughtonscott@gmail.com>
2017-06-26 08:51:25 -0400

    Configured the data that will be passed into the tableview. Passed somedata into the table

--- SocialNetwork/PostCell.swift
+++ SocialNetwork/PostCell.swift
@@ -17,9 +17,23 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
 
+
+    var post: Post!
+
     override func awakeFromNib() {
         super.awakeFromNib()
 
+
+    }
+
+
+    //configures data we will put in the cell. This is called in the cellForRowAt function.
+    
+    func configureCell(post: Post) {
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+        
 
     }
 
